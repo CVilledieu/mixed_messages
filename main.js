811234520b084b9833ef1be69b7b9e90b7408a23
Christopher V <ChrisRVill@gmail.com>
2023-04-06 22:39:58 -0700

    Created MixedMessages class

---
+++ main.js
@@ -0,0 +1,28 @@
+//Mixed messages
+//Every time a user runs a program,
+//they should get a new, randomized output.
+
+
+    const messages = [];
+    const structureArr = [`Hello ${subjectArr}`, `Today's message is brought to you by ${subjectArr}`, `${subjectArr} is going to have a great day`];
+    const subjectArr = [`the world`, `raid shadow legends`, `James may`];
+
+class MixedMessages {
+    constructor (){
+        _structure = [];
+        _subject = [];
+        _constructedMessage = [];
+        
+    }
+
+
+    generateMessage(){
+        
+    }
+
+
+    print(){
+        console.log(generateMessage());
+    }
+
+}
